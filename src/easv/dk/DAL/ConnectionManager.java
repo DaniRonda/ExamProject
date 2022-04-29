dc4b398052a5d7eed5eae7018957d08cd835c88b
DaniRonda <dani68h5@easv365.dk>
2022-04-29 21:16:05 +0200

    Connection manager class and test

---
+++ src/easv/dk/DAL/ConnectionManager.java
@@ -0,0 +1,32 @@
+package easv.dk.DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import java.sql.Connection;
+
+public class ConnectionManager {
+    private final SQLServerDataSource ds;
+    private static ConnectionManager instance;
+
+    public ConnectionManager()
+    {
+        ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("Assignment2-march2022");
+        ds.setPortNumber(1433);
+        ds.setUser("CSe21B_9");
+        ds.setPassword("CSe21B_9");
+    }
+
+    public static ConnectionManager getInstance() throws Exception {
+        return instance == null ? instance = new ConnectionManager() : instance;
+    }
+
+    public static Connection getConnection() throws Exception {
+        return getInstance().ds.getConnection();
+    }
+    public static void main (String[] args) throws Exception {
+        ConnectionManager ds = new ConnectionManager();
+        try (Connection connection= ds.getConnection()){
+            System.out.println("Is it open"+!connection.isClosed());}
+    }
+}
