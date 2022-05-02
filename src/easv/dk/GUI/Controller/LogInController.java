77425f7f9e4af4e448c6e1c9f8870e7a5647505c
Chantal1208 <chan2292@easv365.dk>
2022-05-02 12:10:23 +0200

    Added empty screens + cancel Log in

--- src/easv/dk/GUI/Controller/LogInController.java
+++ src/easv/dk/GUI/Controller/LogInController.java
@@ -1,4 +1,30 @@
 package easv.dk.GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
 public class LogInController {
+
+    @FXML
+    private Button BtnCancel;
+
+    public static class AdminController {
+    }
+
+    public void cancelLogIn(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Cancel login?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            Stage stage = (Stage) BtnCancel.getScene().getWindow();
+            stage.close();
+        }
+
+    }
 }
+
--- src/easv/dk/Main.java
+++ src/easv/dk/Main.java
@@ -1,8 +1,24 @@
 package easv.dk;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+
+public class Main extends Application {
     public static void main(String[] args) {
-	// hallo
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/View/LogInView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
