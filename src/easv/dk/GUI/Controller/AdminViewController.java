c179bf1472ad78839ae8a45018c8d2a9f11d1226
DaniRonda <dani68h5@easv365.dk>
2022-05-12 09:57:59 +0200

    Edit users windows

--- src/easv/dk/GUI/Controller/AdminViewController.java
+++ src/easv/dk/GUI/Controller/AdminViewController.java
@@ -1,14 +1,13 @@
 package easv.dk.GUI.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class AdminViewController {
 
@@ -19,7 +18,7 @@ public class AdminViewController {
 
     public void openAdminEditView (ActionEvent actionEvent) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("easv/dk/GUI/View/AdminEditView.fxml"));
+        loader.setLocation(getClass().getClassLoader().getResource("easv/dk/GUI/View/EditAdminView.fxml"));
         Parent root = loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
---
+++ src/easv/dk/GUI/Controller/EditSchoolViewController.java
@@ -0,0 +1,17 @@
+package easv.dk.GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditSchoolViewController {
+    public TextField schoolName_txt;
+    public Button confirmUpdateSchoolBTN;
+    public Button cancelUpdateSchoolBTN;
+
+    public void confirmUpdateSchool(ActionEvent actionEvent) {
+    }
+
+    public void cancelUpdateSchool(ActionEvent actionEvent) {
+    }
+}
