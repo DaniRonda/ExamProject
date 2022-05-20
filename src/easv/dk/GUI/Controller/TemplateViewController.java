d6e1646633cab1c8d26b10dc5399bd4553dba571
Mareike0308 <mare1702@easv365.dk>
2022-05-20 15:09:05 +0200

    fixed error

--- src/easv/dk/GUI/Controller/TemplateViewController.java
+++ src/easv/dk/GUI/Controller/TemplateViewController.java
@@ -6,10 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-import java.awt.*;
+
 
 public class TemplateViewController {
 
